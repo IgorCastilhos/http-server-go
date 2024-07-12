a3f97c67a983427ed42ec97fbabf39a8a549a15b
= <=>
2024-07-12 17:41:00 -0300

    pass 2st stage

--- app/server.go
+++ app/server.go
@@ -1,45 +1,21 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
 	"os"
-	"strings"
 )
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	// Read the request
-	reader := bufio.NewReader(conn)
-	request, err := reader.ReadString('\n')
+	// Construct the HTTP response
+	response := "HTTP/1.1 200 OK\n\n\r\n"
+
+	// Write the response to the connection
+	_, err := conn.Write([]byte(response))
 	if err != nil {
-		fmt.Println("Error reading request:", err)
-		return
-	}
-
-	// Log the request (optional)
-	fmt.Println("Received request:", strings.TrimSpace(request))
-
-	// Set HTTP version and status line
-	statusLine := "HTTP/1.1 200 OK\r\n"
-
-	// Set headers
-	headers := "Content-Type: text/plain\r\n"
-	headers += "Custom-Header: CustomValue\r\n"
-
-	// End headers with CRLF
-	headers += "\r\n"
-
-	// Set body
-	body := "Hello, this is the body of the response.\r\n"
-
-	// Write response to the connection
-	response := statusLine + headers + body
-	_, err = conn.Write([]byte(response))
-	if err != nil {
-		fmt.Println("Error writing response:", err)
+		fmt.Println("Error writing to connection: ", err)
 		return
 	}
 }
@@ -54,12 +30,14 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+		go handleConnection(conn)
 	}
-
-	go handleConnection(conn)
 }
