5aaf09212ca556cc7f75dc9283133d6970e491e3
= <=>
2024-07-13 17:17:16 -0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -9,7 +9,6 @@ import (
 )
 
 var (
-	// Limit the number of concurrent connections
 	maxConnections = 5
 	semaphore      = make(chan struct{}, maxConnections)
 	filesDirectory string
@@ -19,7 +18,6 @@ func handleConnection(conn net.Conn) {
 
 	defer conn.Close()
 
-	// Read the request
 	buffer := make([]byte, 4096)
 	n, err := conn.Read(buffer)
 	if err != nil {
@@ -34,9 +32,7 @@ func handleConnection(conn net.Conn) {
 	method := parts[0]
 	path := parts[1]
 
-	response := ""
 	userAgent := ""
-
 	for _, line := range lines[1:] {
 		if strings.HasPrefix(line, "User-Agent: ") {
 			userAgent = line[len("User-Agent: "):]
@@ -44,6 +40,15 @@ func handleConnection(conn net.Conn) {
 		}
 	}
 
+	gzipSupported := false
+	for _, line := range lines {
+		if strings.HasPrefix(line, "Accept-Encoding:") && strings.Contains(line, "gzip") {
+			gzipSupported = true
+			break
+		}
+	}
+
+	response := ""
 	if strings.HasPrefix(path, "/files/") {
 		filename := path[len("/files/"):]
 
@@ -64,42 +69,41 @@ func handleConnection(conn net.Conn) {
 			fileContents, err := os.ReadFile(filePath)
 
 			if err != nil {
-				// File does not exist
 				conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 				return
 			}
 
-			// File exists, serve it
 			conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n", len(fileContents))))
 			conn.Write(fileContents)
 		}
 	} else if path == "/user-agent" && userAgent != "" {
 
-		// Prepare the response body
 		responseBody := userAgent
-		// Calculate the Content-Length
 		contentLength := len(responseBody)
 
-		// Construct the response with Content-Type and Content-Length headers
 		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", contentLength, responseBody)
 	} else if strings.HasPrefix(path, "/echo/") {
-		// Extract the string after /echo/
 		echoStr := path[len("/echo/"):]
 
-		// Prepare the response body
 		responseBody := echoStr
-		// Calculate the Content-Length
 		contentLength := len(responseBody)
 
-		// Construct the response with Content-Type and Content-Length headers
-		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", contentLength, responseBody)
+		if gzipSupported {
+			response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: %d\r\n\r\n%s", contentLength, responseBody)
+		} else {
+			response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", contentLength, responseBody)
+		}
+
 	} else if path == "/index.html" || path == "/" {
-		response = "HTTP/1.1 200 OK\r\n\r\n"
+		if gzipSupported {
+			response = "HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\n\r\n"
+		} else {
+			response = "HTTP/1.1 200 OK\r\n\r\n"
+		}
 	} else {
 		response = "HTTP/1.1 404 Not Found\r\n\r\n"
 	}
 
-	// Write the response to the connection
 	_, err = conn.Write([]byte(response))
 	if err != nil {
 		fmt.Println("Error writing to connection: ", err)
@@ -108,19 +112,15 @@ func handleConnection(conn net.Conn) {
 }
 
 func main() {
-	// Parse the --directory flag
 	flag.StringVar(&filesDirectory, "directory", ".", "the directory to serve files from")
 	flag.Parse()
 
-	// Ensure the directory flag is set
 	if filesDirectory == "" {
 		fmt.Println("Please provide the directory to serve files from using the -directory flag")
 		os.Exit(1)
 	}
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -134,7 +134,6 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			continue
 		}
-		// Acquire a semaphore before handling the connection
 		semaphore <- struct{}{}
 
 		go handleConnection(conn)
